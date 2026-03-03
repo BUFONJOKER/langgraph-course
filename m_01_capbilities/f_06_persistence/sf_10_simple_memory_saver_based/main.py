ff95d7b2db43605d01926dc5b5e575502ee4b183
Abdul Rehman Javaid <134438770+BUFONJOKER@users.noreply.github.com>
2026-03-03 19:55:52 +0500

    11 videos completed

--- m_01_capbilities/f_06_persistence/sf_10_simple_memory_saver_based/main.py
+++ m_01_capbilities/f_06_persistence/sf_10_simple_memory_saver_based/main.py
@@ -3,6 +3,7 @@ from generate_joke_explanation import generate_joke_explanation
 from model import load_model
 from state_schema import JokeState
 from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import InMemorySaver
 
 model = load_model()
 
@@ -15,15 +16,25 @@ graph.add_edge(START, 'joke')
 graph.add_edge('joke', 'explanation')
 graph.add_edge('explanation', END)
 
-workflow = graph.compile()
+checkpointer = InMemorySaver()
 
+workflow= graph.compile(checkpointer=checkpointer)
 
+config1 = {
+  'configurable':{'thread_id':'1'}
+}
 
 initial_state = {'topic':"Data", 'model':model}
 
-result = workflow.invoke(input=initial_state)
+result = workflow.invoke(input=initial_state, config=config1)
 
-for key, val in result.items():
-  print()
-  print(f"----------{key.capitalize()}-----------")
-  print(val)
+# print(workflow.get_state(config1))
+
+state_history = list(workflow.get_state_history(config1))
+
+# print(state_history[-3])
+
+# for key, val in result.items():
+#   print()
+#   print(f"----------{key.capitalize()}-----------")
+#   print(val)
